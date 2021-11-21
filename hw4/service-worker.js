06238367e0c64043155a1fa5ded566e8d6b0391c
devonbow <devonbow@hotmail.com>
2021-11-21 01:32:28 -0800

    Add files via upload

---
+++ hw4/service-worker.js
@@ -0,0 +1,7 @@
+self.oninstall = function () {
+    caches.open('fayeFrontEndV1').then(function (cache) {
+            cache.addAll([/ ... /])
+                .catch(/ ... /);
+        })
+        .catch(/ ... /)
+}
